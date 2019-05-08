998336cc8dc6b942d654941a181c4a50a2339844
Aymeric Galan <aymeric.galan@gmail.com>
2019-05-08 08:24:54 +0200

    Add Dds_Ds correction + MultiMass now inherits from the base class

--- pylensmodels/mass/multi.py
+++ pylensmodels/mass/multi.py
@@ -7,15 +7,17 @@ from pylensmodels.mass.base import BaseMassModel
 from pylensmodels.utils import lensing
 
 
-class MultiMass(object):
+class MultiMass(BaseMassModel):
 
-    def __init__(self, model_list):
+    def __init__(self, model_list, Dds_Ds=None):
+        self.model_list = model_list
         self.models = []
         for model in model_list:
             if isinstance(model, BaseMassModel):
                 self.models.append(model)
             else:
                 print("WARNING : model {} is not an instance of the base class (model ignored)")
+        self._Dds_Ds = Dds_Ds
 
     def potential(self, x, y):
         pot = 0.
@@ -39,29 +41,37 @@ class MultiMass(object):
             f_yy += f_yy_
             f_xy += f_xy_
             f_yx += f_yx_
+
+        if self._Dds_Ds is not None:
+            # scale second derivatives by the distance ratio Dds/Ds
+            f_xx *= self._Dds_Ds
+            f_yy *= self._Dds_Ds
+            f_xy *= self._Dds_Ds
+            f_yx *= self._Dds_Ds
+
         return f_xx, f_yy, f_xy, f_yx
 
-    def deflection(self, x, y):
-        alpha1, alpha2 = 0., 0.
-        for model in self.models:
-            alpha1_, alpha2_ = model.deflection(x, y)
-            alpha1 += alpha1_
-            alpha2 += alpha2_
-        return alpha1, alpha2
+    # def deflection(self, x, y):
+    #     alpha1, alpha2 = 0., 0.
+    #     for model in self.models:
+    #         alpha1_, alpha2_ = model.deflection(x, y)
+    #         alpha1 += alpha1_
+    #         alpha2 += alpha2_
+    #     return alpha1, alpha2
 
-    def shear(self, x, y):
-        gamma1, gamma2 = 0., 0.
-        for model in self.models:
-            gamma1_, gamma2_ = model.shear(x, y)
-            gamma1 += gamma1_
-            gamma2 += gamma2_
-        return gamma1, gamma2
+    # def shear(self, x, y):
+    #     gamma1, gamma2 = 0., 0.
+    #     for model in self.models:
+    #         gamma1_, gamma2_ = model.shear(x, y)
+    #         gamma1 += gamma1_
+    #         gamma2 += gamma2_
+    #     return gamma1, gamma2
 
-    def convergence(self, x, y):
-        kappa = 0.
-        for model in self.models:
-            kappa += model.convergence(x, y)
-        return kappa
+    # def convergence(self, x, y):
+    #     kappa = 0.
+    #     for model in self.models:
+    #         kappa += model.convergence(x, y)
+    #     return kappa
 
     def derivative_numdiff(self, x, y, diff=1e-7, method='2-points'):
         """using numerical differentiation from 1st order derivatives"""
@@ -92,11 +102,6 @@ class MultiMass(object):
         else:
             raise NotImplementedError("Only the 2-points method is currently implemented")
         return f_xx, f_yy, f_xy, f_yx
-
-    def magnification(self, x, y):
-        """return magnification map"""
-        f_xx, f_yy, f_xy, f_yx = self.hessian(x, y)
-        return lensing.hessian2mag(f_xx, f_yy, f_xy, f_yx)
 
     def magnification_numdiff(self, x, y, diff=1e-7):
         """return magnification map"""
